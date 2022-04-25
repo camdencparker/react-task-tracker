047f71daca83a00db888b8b5861f220f1eb0433c
Camden <camdencparker@gmail.com>
2022-04-25 11:27:58 -0700

    allows to remove tasks from list

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ function App() {
     { id: 2, text: "feed fish", time: "Wednesday" },
     { id: 3, text: "watch F1", time: "Sunday" },
   ]);
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
