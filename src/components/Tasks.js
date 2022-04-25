9c6c7fa8856eeffb8db30b54a7d79e1a5b5ea8f0
Camden <camdencparker@gmail.com>
2022-04-25 11:04:27 -0700

    useState hook integrated to tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,11 @@
-const tasks = [{ text: 1 }, { text: 2 }, { text: 3 }];
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "mow lawn" },
+    { id: 2, text: "feed fish" },
+    { id: 3, text: "watch F1" },
+  ]);
   return (
     <>
       {tasks.map((task) => (
