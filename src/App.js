3d9c068b8b9dc7427ecf6bd8820d407eeaef882c
Camden <camdencparker@gmail.com>
2022-04-25 11:37:07 -0700

    uses bollean logic to toggle if task has reminder

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Tasks from "./components/Tasks";
 
 function App() {
   const [tasks, setTasks] = useState([
-    { id: 1, text: "mow lawn", time: "Tuesday" },
-    { id: 2, text: "feed fish", time: "Wednesday" },
-    { id: 3, text: "watch F1", time: "Sunday" },
+    { id: 1, text: "mow lawn", time: "Tuesday", reminder: true },
+    { id: 2, text: "feed fish", time: "Wednesday", reminder: true },
+    { id: 3, text: "watch F1", time: "Sunday", reminder: false },
   ]);
 
   //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
     </div>
   );
 }
