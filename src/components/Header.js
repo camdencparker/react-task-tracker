490629c029bcef366ab7216220b11656d47e41a1
Camden <camdencparker@gmail.com>
2022-04-25 10:51:49 -0700

    event added to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import Button from "./Button";
 const Header = () => {
+  const onClick = () => {
+    console.log("Click");
+  };
   return (
     <div>
       <header className="header">
         <h1>Task Tracker</h1>
-        <Button color="green" text="Hello" />
+        <Button color="green" text="Add" onClick={onClick} />
       </header>
     </div>
   );
